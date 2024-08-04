bcd960f7367046b0ad132a67f79f4a2a6294dc0f
kjbowman00 <kj.bowman00@gmail.com>
2024-08-04 12:50:19 -0400

    Basic websockets transmitting data

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
-import express from 'express';
-import _expressWs from 'express-ws';
+import { WebSocketServer } from 'ws';
 
-// To get types, have to get app from expressWs
-// const app = express();
-const socket = _expressWs(express());
-const app = socket.app;
+const wss = new WebSocketServer({ port: 8080 });
 
-console.log("STARTING UP");
+wss.on('connection', function connection(ws) {
+  console.log("HELLO WORLD");
+  ws.on('error', console.error);
 
-app.listen(8080, () =>
-  console.log('Example app listening on port 8080!'),
-);
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
 
-
-app.ws('/echo', function(ws, req) {
-    ws.on('message', function(msg) {
-        console.log("MESSAGE: ", msg);
-        ws.send(msg);
-    });
+  ws.send('something');
 });
